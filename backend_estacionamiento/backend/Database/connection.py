63a4fecc3ac19acef40cfe7e4874e19b76ec78f2
Luciano1503 <odarlucianocondori@gmail.com>
2026-05-17 15:18:28 -0500

    Update database connection logic for cloud support

--- backend_estacionamiento/backend/Database/connection.py
+++ backend_estacionamiento/backend/Database/connection.py
@@ -7,13 +7,21 @@ from psycopg2.extras import RealDictCursor
 
 ENV_PATH = Path(__file__).resolve().parents[3] / ".env"
 
-
 def get_connection():
     load_dotenv(ENV_PATH)
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        database=os.getenv("DB_NAME", "ProyectoIntegrador"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD"),
-        cursor_factory=RealDictCursor,
-    )
+    
+    # 1. Intentamos buscar la URL mágica de Railway primero
+    db_url = os.getenv("DATABASE_URL")
+    
+    if db_url:
+        # Si encuentra DATABASE_URL (está en la nube), se conecta directo
+        return psycopg2.connect(db_url, cursor_factory=RealDictCursor)
+    else:
+        # Si NO la encuentra (estás en tu laptop), usa tus variables locales
+        return psycopg2.connect(
+            host=os.getenv("DB_HOST", "localhost"),
+            database=os.getenv("DB_NAME", "ProyectoIntegrador"),
+            user=os.getenv("DB_USER", "postgres"),
+            password=os.getenv("DB_PASSWORD"),
+            cursor_factory=RealDictCursor,
+        )
